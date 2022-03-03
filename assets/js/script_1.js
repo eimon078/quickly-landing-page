53420ffff92044f984f636f85cb954218421ec9f
eimon078 <eimonhossain078@gmail.com>
2022-03-03 20:56:10 +0600

    Added navbar without responsive

---
+++ assets/js/script_1.js
@@ -0,0 +1,6 @@
+var toggler = document.getElementById("toggle-container");
+var toggleContents = document.getElementById("toggle-content");
+document.addEventListener("click", function(){
+//Apply a class intance to each navigation and set display to toggle: 
+     toggleContents.classList.toggle("displayed");
+});
